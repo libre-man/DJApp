cd97820f1ee947c203c963ab3aeb9b0aaf37b52b
Devin Hillenius <devinhillenius@quicknet.nl>
2016-09-22 12:25:04 +0200

    New android (API 19) project created instead of the API 21 project

--- SDaaS/app/src/main/java/nl/sdaas/app/sdaas/SessionActivity.java
+++ SDaaS/app/src/main/java/nl/sdaas/app/sdaas/SessionActivity.java
@@ -1,13 +1,13 @@
-package nl.sdaas.android.sdaas;
+package nl.sdaas.app.sdaas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SessionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_session);
     }
 }

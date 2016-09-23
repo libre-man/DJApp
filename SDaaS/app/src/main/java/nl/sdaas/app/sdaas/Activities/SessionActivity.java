acc6b0640e9e92d63a9eb45bccd361da038b42b1
Devin Hillenius <devinhillenius@quicknet.nl>
2016-09-23 13:49:03 +0200

    Create Parser class that functions for now

--- SDaaS/app/src/main/java/nl/sdaas/app/sdaas/Activities/SessionActivity.java
+++ SDaaS/app/src/main/java/nl/sdaas/app/sdaas/Activities/SessionActivity.java
@@ -2,20 +2,22 @@ package nl.sdaas.app.sdaas.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import org.json.*;
 
 import nl.sdaas.app.sdaas.ChannelAdapter;
+import nl.sdaas.app.sdaas.Parser;
 import nl.sdaas.app.sdaas.R;
 import nl.sdaas.app.sdaas.Session;
 
 public class SessionActivity extends AppCompatActivity {
 
-    private Session session;
+    private final static String TAG = SessionActivity.class.getName();
 
-    private String[] testListView = {"Test0", "Test1", "Test2"};
+    private Session session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,39 +36,18 @@ public class SessionActivity extends AppCompatActivity {
                 "    \"session_name\": \"CoolDisco\" \n" +
                 "}";
 
-        try {
-            this.parseResponse(dummyResponse);
+        if ((this.session = Parser.parseInitialSessionResponse(dummyResponse)) == null) {
+            Log.d(TAG, "Cannot parse session");
+            return;
+        }
 
-            System.out.println("Test");
+        ChannelAdapter adapter = new ChannelAdapter(this, this.session);
 
-            ChannelAdapter adapter = new ChannelAdapter(this, this.session);
+        ListView listView = (ListView) findViewById(R.id.channelListView);
 
-            ListView listView = (ListView) findViewById(R.id.channelListView);
+        if (listView != null)
             listView.setAdapter(adapter);
-        } catch (JSONException e) {
-            System.out.println("Fuck");
-            e.printStackTrace();
-        }
     }
 
-    private boolean parseResponse(String jsonString) throws JSONException {
-        JSONObject sessionInfo = new JSONObject(jsonString);
-
-        if (sessionInfo.getBoolean("success")) {
-            /* Get the session name. */
-            session = new Session(sessionInfo.getString("session_name"));
-
-            /* Get the channels in the session. */
-            JSONArray channels = sessionInfo.getJSONArray("channels");
-            for (int i = 0; i < channels.length(); i++) {
-                JSONObject channel = channels.getJSONObject(i);
-                session.addChannel(channel.getInt("color"), channel.getInt("channel_id"),
-                        channel.getString("url"));
-            }
-            return true;
-        }
-
-        throw new JSONException("Unsuccessful");
-    }
 
 }

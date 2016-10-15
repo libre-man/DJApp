e95e495d6b7e5f463861bef07b364aa4b585b2d3
Devin Hillenius <devinhillenius@quicknet.nl>
2016-10-15 18:18:31 +0200

    Initial try for detecting Headphone Button clicks, not working on my phone, maybe it does on the Pizza Hawaii phone

--- SDaaS/app/src/main/java/nl/sdaas/app/sdaas/services/SdaasService.java
+++ SDaaS/app/src/main/java/nl/sdaas/app/sdaas/services/SdaasService.java
@@ -1,7 +1,10 @@
 package nl.sdaas.app.sdaas.services;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
@@ -14,9 +17,11 @@ import nl.sdaas.app.sdaas.Logger;
 public class SdaasService extends Service {
     private final static String TAG = SdaasService.class.getName();
     private Logger logger;
+    private HeadphoneButtonReceiver receiver;
 
     @Override
     public void onDestroy() {
+        unregisterReceiver(this.receiver);
         super.onDestroy();
     }
 
@@ -32,20 +37,37 @@ public class SdaasService extends Service {
     public void onCreate() {
         super.onCreate();
         this.logger = new Logger();
-
+        this.receiver = new HeadphoneButtonReceiver();
+        /* Set up Logging thread! */
         Thread loggingThread = new Thread(new Runnable() {
             @Override
             public void run() {
                 SdaasService.this.logger.intervalLogger();
             }
         });
-
         loggingThread.start();
+
+        IntentFilter filter = new IntentFilter(Intent.ACTION_MEDIA_BUTTON);
+        filter.setPriority(Integer.MAX_VALUE);
+        this.registerReceiver(this.receiver, filter);
     }
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    public static  class HeadphoneButtonReceiver extends BroadcastReceiver {
+
+        public HeadphoneButtonReceiver() {
+            super();
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String intentAction = intent.getAction();
+            System.out.println("BUTTON PRESS:" + intentAction);
+        }
     }
 }

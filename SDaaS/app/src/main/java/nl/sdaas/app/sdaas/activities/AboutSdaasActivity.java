4d88e443a523b1c87854695f236692fd5976e7e6
Devin Hillenius <devinhillenius@quicknet.nl>
2017-01-23 20:14:20 +0100

    Make a start for the help / about sdaas activity

---
+++ SDaaS/app/src/main/java/nl/sdaas/app/sdaas/activities/AboutSdaasActivity.java
@@ -0,0 +1,10 @@
+package nl.sdaas.app.sdaas.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Devinhillenius on 23/01/17.
+ */
+
+public class AboutSdaasActivity extends AppCompatActivity {
+}
